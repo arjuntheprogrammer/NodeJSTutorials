48677dfe19596244b4fc0675e94315b655bdea0e
Arjun Gupta <saimzn4241@gmail.com>
2017-08-29 17:49:37 +0530

    init

---
+++ blocking.js
@@ -0,0 +1,7 @@
+//example shows that the program blocks
+//until it reads the file and then only it proceeds
+// to end the program.
+var fs = require('fs');
+var data = fs.readFileSync('input.txt');
+console.log(data.toString());
+console.log("Program ended");
