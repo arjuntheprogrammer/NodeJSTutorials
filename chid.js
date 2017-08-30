c4fab8f9148ef4e5d01cf3f89d424804c0d41705
Arjun Gupta <saimzn4241@gmail.com>
2017-08-30 21:08:53 +0530

    child

---
+++ chid.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const child_process = require('child_process');
+
+for(var i=0; i<3; i++){
+  var workerProcess = child_process.exec('node support.js ' +i, function(error, stdout, stderr){
+    if(error){
+      console.log(error.stack);
+      console.log('Error code:' + error.code);
+      console.log('Signal Recieved:'+ error.signal);
+    }
+    console.log('stdout:' + stdout);
+    console.log('stderr:' + stderr);
+
+  });
+  workerProcess.on('exit', function(code){
+    console.log('Child Process exited with exit code' + code);
+  });
+}
