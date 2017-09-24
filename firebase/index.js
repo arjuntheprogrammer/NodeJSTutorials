fa1c0fcc27b753b5bd72c91d6555e10581995b80
Arjun Gupta <saimzn4241@gmail.com>
2017-09-24 14:44:39 +0530

    snap onchange

--- firebase/index.js
+++ firebase/index.js
@@ -7,10 +7,42 @@ firebase.initializeApp({
 
 var ref = firebase.database().ref('node-client');
 var messagesRef = ref.child('messages');
+// var messageRef = messagesRef.push();
+// var messageKey = messageRef.key;
+// var payload = {};
 
-messagesRef.push({
-    name: 'Arjun',
-    admin: true,
-    count: 1,
-    text: 'Hey guys'
-});
+// var message = {
+//     text: 'hey guys'
+// };
+// payload['userMessages/' + messageKey] = message;
+// payload['logs/' + messageKey] = message;
+
+// ref.update(payload);
+
+// ref.once('value').then(function(snap) {
+//     console.log(snap.key, "\n\n");
+//     console.log(snap.ref.toString(), "\n\n");
+//     console.log(snap.val(), "\n\n");
+// });
+
+
+
+// var logs;
+// ref.child('logs').on('value', function(snap) {
+//     logs = snap.val();
+//     console.log(logs);
+// });
+
+
+// messagesRef.push({
+//     name: 'Arjun',
+//     admin: true,
+//     count: 1,
+//     text: 'Hey guys'
+// });
+
+
+// ref.update({
+//     'messages/some/child': 1,
+//     'messages/another/child': 2
+// })
